45d53fa983fede68cc51232f2fab1742630380cf
Alan Reis <alanreisanjo@gmail.com>
2025-05-18 14:22:46 -0300

    Preparativos CI/CD - Deploy contínuo na Vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Body, HTML, Navbar } from "@/components/ui";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata: Metadata = {
 	title: "Create Next App",
@@ -18,6 +19,7 @@ export default function RootLayout({
 				{/* <Navbar.Root>
 					<Navbar.MadeBy />
 				</Navbar.Root> */}
+				<SpeedInsights />
 			</Body>
 		</HTML>
 	);
