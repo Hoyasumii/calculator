911440910638be64e691c9bc1906c4e951d52e4e
Alan Reis <alanreisanjo@gmail.com>
2025-05-19 01:13:26 -0300

    Será?

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,22 +1,22 @@
 "use client";
 
-import { BackspaceButton } from "@/components/BackspaceButton";
-import { ControlButton } from "@/components/ControlButton";
-import { NumberButton } from "@/components/NumberButton";
-import {
-	Diff,
-	Divide,
-	Minus,
-	Plus,
-	X,
-} from "lucide-react";
+// import { BackspaceButton } from "@/components/BackspaceButton";
+// import { ControlButton } from "@/components/ControlButton";
+// import { NumberButton } from "@/components/NumberButton";
+// import {
+// 	Diff,
+// 	Divide,
+// 	Minus,
+// 	Plus,
+// 	X,
+// } from "lucide-react";
 
 export default function Page() {
 	return (
 		<div className="rounded-2xl dark:bg-stone-900 pb-3 border-[0.5px] dark:border-stone-600/20 overflow-hidden">
 			<div className="py-3 min-h-16 w-full dark:bg-stone-900 border-b-[0.5px] dark:border-stone-600/20">{" "}</div>
 			<div className="grid grid-cols-4 gap-2 p-3 pb-0">
-				<BackspaceButton />
+				{/* <BackspaceButton />
 				<ControlButton>
 					<Diff />
 				</ControlButton>
@@ -46,7 +46,7 @@ export default function Page() {
 				</ControlButton>
 				<ControlButton>,</ControlButton>
 				<NumberButton>0</NumberButton>
-				<ControlButton>=</ControlButton>
+				<ControlButton>=</ControlButton> */}
 			</div>
 		</div>
 	);
