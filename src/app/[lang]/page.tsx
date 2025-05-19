d0cdbb11fb28be77f8c41b4f46a8420bbac4014e
Alan Reis <alanreisanjo@gmail.com>
2025-05-19 00:39:40 -0300

    Que projeto chato da porra

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,3 +1,56 @@
+"use client";
+
+import { BackspaceButton } from "@/components/BackspaceButton";
+import { ControlButton } from "@/components/ControlButton";
+import { NumberButton } from "@/components/NumberButton";
+import {
+	Delete,
+	Diff,
+	Divide,
+	Equal,
+	Minus,
+	Percent,
+	Plus,
+	X,
+} from "lucide-react";
+
 export default function Page() {
-  return <h1>Hello World</h1>
+	return (
+		<div className="rounded-2xl dark:bg-stone-900 pb-3 border-[0.5px] dark:border-stone-600/20 overflow-hidden">
+			<div className="py-3 min-h-16 w-full dark:bg-stone-900 border-b-[0.5px] dark:border-stone-600/20">{" "}</div>
+			<div className="grid grid-cols-4 gap-2 p-3 pb-0">
+				<BackspaceButton />
+				<ControlButton>
+					<Diff />
+				</ControlButton>
+				<NumberButton>
+					!
+				</NumberButton>
+				<ControlButton>
+					<Divide />
+				</ControlButton>
+				<NumberButton>1</NumberButton>
+				<NumberButton>2</NumberButton>
+				<NumberButton>3</NumberButton>
+				<ControlButton>
+					<X />
+				</ControlButton>
+				<NumberButton>4</NumberButton>
+				<NumberButton>5</NumberButton>
+				<NumberButton>6</NumberButton>
+				<ControlButton>
+					<Minus />
+				</ControlButton>
+				<NumberButton>7</NumberButton>
+				<NumberButton>8</NumberButton>
+				<NumberButton>9</NumberButton>
+				<ControlButton>
+					<Plus />
+				</ControlButton>
+				<ControlButton>,</ControlButton>
+				<NumberButton>0</NumberButton>
+				<ControlButton>=</ControlButton>
+			</div>
+		</div>
+	);
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -47,7 +47,7 @@ export default function RootLayout({
 				<Navbar.Root DarkLogo={LogoDark} LightLogo={LogoLight}>
 					<Navbar.MadeBy />
 				</Navbar.Root>
-				<Main>{children}</Main>
+				<Main className="">{children}</Main>
 				<SpeedInsights />
 			</Body>
 		</HTML>
