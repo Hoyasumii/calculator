13fbd0f39361e03c58367918de5a5cf797eec615
Alan Reis <alanreisanjo@gmail.com>
2025-05-19 01:09:13 -0300

    ?

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,7 +37,7 @@ export async function generateMetadata(): Promise<Metadata> {
 	};
 }
 
-export default async function RootLayout({
+export default function RootLayout({
 	children,
 }: Readonly<{
 	children: React.ReactNode;
